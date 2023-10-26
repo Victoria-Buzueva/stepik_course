addfaa1f1151934e482e6e511e8d77b572759bdd
Victoria Buzueva <vika_b@tut.by>
2023-10-27 00:03:22 +0300

    homework from previous 1-6 module

---
+++ pythonlesson_1-6_step4.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://suninjuly.github.io/file_input.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.NAME, "firstname")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "lastname")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.NAME, "email")
+    input3.send_keys("Smolensk")
+
+
+
+    button = browser.find_element(By.CSS_SELECTOR, ".btn.btn-primary")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
